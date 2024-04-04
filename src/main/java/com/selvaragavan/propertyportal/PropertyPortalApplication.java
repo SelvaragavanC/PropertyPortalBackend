d32619e621a0241e373c2d582f8b492826e9e06e
SelvaragavanC <selvaragavan.ct@gmail.com>
2024-04-04 08:58:51 +0530

    Intitalized a spring project with spring web,JDBC API,MYSQL Driver

---
+++ src/main/java/com/selvaragavan/propertyportal/PropertyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.selvaragavan.propertyportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyPortalApplication.class, args);
+    }
+
+}
